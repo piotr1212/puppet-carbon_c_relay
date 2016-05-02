1dc299325733a4519837d361557e05200fb1777f
Jenkins <jenkins@build.paas.intranet>
2016-05-02 11:52:53 +0000

    Automated commit to update module config files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
-  c.default_facts = {
-    :concat_basedir            => '/nonexistant',
-    :osfamily                  => 'Redhat',
-    :operatingsystemmajrelease => '7',
-  }
   c.after(:suite) do
     RSpec::Puppet::Coverage.report!
   end
